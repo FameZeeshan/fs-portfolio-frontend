e41cd58d0046696e86341e16c478150e080cbb51
Mohammed Zeeshan <zeeshan6143@gmail.com>
2023-12-07 17:07:40 +0530

    resume and projects updated

--- src/components/About.js
+++ src/components/About.js
@@ -7,20 +7,17 @@ export default function About() {
         <h1>About</h1>
         <h3>I'm Mohammed Zeeshan</h3>
         <p>
-          I am a Full Stack Developer with expertise in HTML, CSS, JavaScript,
-          Bootstrap, and popular frameworks such as React.js. I create visually
-          appealing and user-friendly interfaces with a focus on detail and stay
-          up-to-date with the latest frontend technologies and best practices. I
-          am skilled in troubleshooting and have a good understanding of web
-          development concepts. I have experience working with backend
-          technologies such as Node.js and Express.js. I am constantly learning
-          and expanding my skillset to stay ahead in the rapidly evolving tech
-          industry. In addition to my technical skills, I am a team player and
-          have excellent communication and problem-solving skills. I thrive in
-          collaborative environments and enjoy working with others to achieve
-          common goals. If you have any questions or would like to collaborate
-          on a project, feel free to get in touch with me. I look forward to
-          connecting with you!
+          Hello, I'm Mohammed Zeeshan, a Full Stack Developer skilled in HTML,
+          CSS, JavaScript, Bootstrap, and React.js. I specialize in creating
+          visually appealing and user-friendly interfaces. Proficient in both
+          frontend and backend technologies, including Node.js and Express.js, I
+          am committed to staying ahead in the ever-evolving tech industry.
+          <br></br>
+          <br></br>I also have a strong interest in Excel automation using
+          Python and libraries such as Pandas, NumPy, and Openpyxl. This
+          additional expertise enables me to streamline data processes
+          effectively. I am a team player with excellent communication and
+          problem-solving skills, thriving in collaborative environments.
         </p>
         <div className="button-container">
           <ScrollLink
--- src/components/Project.js
+++ src/components/Project.js
@@ -3,6 +3,8 @@ import "./style.css";
 import accordion from "../images/accordion.png";
 import demo1 from "../images/demo1.png";
 import formvalidation from "../images/form-validation.png";
+import pdfhighlighter from "../images/pdf highlighter.png";
+import expensetracker from "../images/expnese tracker.png";
 export default function Project() {
   return (
     <div className="project">
@@ -23,7 +25,7 @@ export default function Project() {
               </a>
               <a
                 className="btn btn-primary"
-                href="https://tranquil-crumble-bdb1b8.netlify.app/"
+                href="https://famezeeshan-accordion-react-project.netlify.app/"
                 role="button"
               >
                 Demo
@@ -42,7 +44,7 @@ export default function Project() {
               </a>
               <a
                 className="btn btn-primary"
-                href="https://ubiquitous-youtiao-d3a8c2.netlify.app/"
+                href="https://famezeeshan-form-react-project.netlify.app/"
                 role="button"
               >
                 Demo
@@ -50,23 +52,39 @@ export default function Project() {
             </div>
           </div>
           <div className="logo-img-container">
-            <img src={demo1} alt="html-logo" />
+            <img src={pdfhighlighter} alt="html-logo" />
             <div className="button-container">
-              <a className="btn btn-primary" href="/" role="button">
+              <a
+                className="btn btn-primary"
+                href="https://github.com/FameZeeshan/pdf-highlighter-app.git"
+                role="button"
+              >
                 Code
               </a>
-              <a className="btn btn-primary" href="/" role="button">
+              <a
+                className="btn btn-primary"
+                href="https://pdf-highlighter.netlify.app/"
+                role="button"
+              >
                 Demo
               </a>
             </div>
           </div>
           <div className="logo-img-container">
-            <img src={demo1} alt="html-logo" />
+            <img src={expensetracker} alt="html-logo" />
             <div className="button-container">
-              <a className="btn btn-primary" href="/" role="button">
+              <a
+                className="btn btn-primary"
+                href="https://github.com/FameZeeshan/Expense-Tracker-React.git"
+                role="button"
+              >
                 Code
               </a>
-              <a className="btn btn-primary" href="/" role="button">
+              <a
+                className="btn btn-primary"
+                href="https://expense-tracker-react-u.netlify.app/"
+                role="button"
+              >
                 Demo
               </a>
             </div>
